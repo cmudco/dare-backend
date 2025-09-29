d6d10c39471311db6caa6ba213d86f65be74f7ce
farhat-is-coding <farhxt.abbxs69@gmail.com>
2025-09-30 00:56:20 +0500

    enhance: auto workflow migration

--- workflows/api/urls.py
+++ workflows/api/urls.py
@@ -6,8 +6,6 @@ from workflows.constants import APP_NAME
 router = DefaultRouter()
 router.register(r'workflows', WorkflowViewSet, basename='workflow')
 router.register(r'workflow-runs', WorkflowRunViewSet, basename='workflow-run')
-router.register(r'workflow-nodes', WorkflowNodeViewSet, basename='workflow-node')
-router.register(r'workflow-edges', WorkflowEdgeViewSet, basename='workflow-edge')
 
 app_name = APP_NAME
 
