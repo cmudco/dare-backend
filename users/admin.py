b970a845498500e185e2bc39cb75509a2ccc4e3a
farhat-is-coding <farhxt.abbxs69@gmail.com>
2025-10-14 20:38:16 +0500

    refactor: admin file of conversations app

--- users/admin.py
+++ users/admin.py
@@ -11,6 +11,7 @@ from billing.services import WalletService
 from django import forms
 from decimal import Decimal
 from users.constants import VectorDBChoice, AuthSourceChoice, ScopeChoice
+from users.filters import LastLoginFilter
 from users.admin_constants import (
     LAST_LOGIN_DISPLAY_RULES,
     LAST_LOGIN_DISPLAY_DEFAULT,
@@ -124,40 +125,6 @@ class AccessCodeGroupAdmin(admin.ModelAdmin):
         return super().has_delete_permission(request, obj)
     
     actions = ["credit_groups_users"]
-
-
-class LastLoginFilter(admin.SimpleListFilter):
-    """Filter users by last login activity"""
-    title = 'last login activity'
-    parameter_name = 'last_login_activity'
-
-    def lookups(self, request, model_admin):
-        return (
-            ('never', 'Never logged in'),
-            ('7days', 'Last 7 days'),
-            ('30days', 'Last 30 days'),
-            ('90days', 'Last 90 days'),
-            ('inactive_30', 'Inactive 30+ days'),
-            ('inactive_90', 'Inactive 90+ days'),
-            ('inactive_180', 'Inactive 180+ days'),
-        )
-
-    def queryset(self, request, queryset):
-        now = timezone.now()
-        if self.value() == 'never':
-            return queryset.filter(last_login__isnull=True)
-        if self.value() == '7days':
-            return queryset.filter(last_login__gte=now - timedelta(days=7))
-        if self.value() == '30days':
-            return queryset.filter(last_login__gte=now - timedelta(days=30))
-        if self.value() == '90days':
-            return queryset.filter(last_login__gte=now - timedelta(days=90))
-        if self.value() == 'inactive_30':
-            return queryset.filter(last_login__lt=now - timedelta(days=30))
-        if self.value() == 'inactive_90':
-            return queryset.filter(last_login__lt=now - timedelta(days=90))
-        if self.value() == 'inactive_180':
-            return queryset.filter(last_login__lt=now - timedelta(days=180))
 
 
 class UserAdmin(DjangoUserAdmin):
